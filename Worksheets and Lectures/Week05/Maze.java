448ea61273013518a8be33e991b381c078e6ef09
Samuel-Wyatt <samuel.wyatt2@outlook.com>
2022-04-05 12:58:17 +0800

    Changed maze values again

--- Worksheets and Lectures/Week05/Maze.java
+++ Worksheets and Lectures/Week05/Maze.java
@@ -16,11 +16,11 @@ public class Maze
     public static void main(String[] args)
     {
         int[][] grid = {
-            { WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL},
+            { WALL,  WALL,  EMPTY,  WALL,  EMPTY,  WALL,  WALL,  WALL,  WALL, WALL},
             {END, EMPTY,  WALL,  WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, WALL},
             { EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY,  WALL, WALL},
             { WALL,  WALL,  WALL,  WALL, EMPTY,  WALL, EMPTY,  WALL, EMPTY, WALL},
-            { WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY, EMPTY, EMPTY, WALL},
+            { WALL, EMPTY, EMPTY, EMPTY, WALL,  WALL, EMPTY, EMPTY, EMPTY, WALL},
             { WALL,  WALL, EMPTY,  WALL,  WALL, EMPTY, EMPTY,  WALL, EMPTY, WALL},
             { WALL,  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  WALL,  WALL, EMPTY,  END},
             { WALL,  WALL,  WALL,  EMPTY,  WALL,  WALL,  WALL,  WALL,  WALL, WALL},
